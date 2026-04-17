e6fa9793f8ca020a20a2a88778f75b1e3714374d
1GoodCod1 <ilieluca200117@gmail.com>
2026-04-17 21:37:33 +0300

    fix(eslint): deprecated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
 import eslint from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    ignores: [
-      'dist/**',
-      'node_modules/**',
-      'coverage/**',
-      'patches/**',
-      '*.config.js',
-      'package-lock.json',
-    ],
-  },
+export default defineConfig(
+  globalIgnores([
+    'dist/**',
+    'node_modules/**',
+    'coverage/**',
+    'patches/**',
+    '*.config.js',
+    'package-lock.json',
+  ]),
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   prettierRecommended,
