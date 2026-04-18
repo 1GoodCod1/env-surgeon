66ba42c117fcf3c009c34f0e63bf5cb3e5968de4
1GoodCod1 <ilieluca200117@gmail.com>
2026-04-18 12:13:40 +0300

    add fixtures for test

---
+++ test/fixtures/src/app.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const dbUrl = process.env.DATABASE_URL;
+const port = process.env['PORT'];
+const apiKey = process.env.API_KEY;
+const debugEnv = process.env.DEBUG;
+const jwtSecret = process.env.JWT_SECRET;
